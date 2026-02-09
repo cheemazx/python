e308d65b450b0348a89937a35a15b80d36fcc493
cheemazx <2012341@szabist-isb.pk>
2026-02-09 23:38:48 +0500

    day0.1

--- variables.py
+++ variables.py
@@ -5,6 +5,10 @@ print(x-y)
 print(x*y)
 print(x/y)
 print(x%y)
+a=2
+A=3
+print("the value of a is ",a)
+print("the value of A is ",A)
 """
 this is a multiline comment"""
 d=10        #this is a single line comment

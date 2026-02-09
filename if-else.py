757c5052b2e70adb0eac92ce46eba1a30f3f60b2
cheemazx <2012341@szabist-isb.pk>
2026-02-09 22:46:40 +0500

    initial commit

---
+++ if-else.py
@@ -0,0 +1,9 @@
+if 5>6:
+    print("5 is greater than 4")
+else:
+    print("5 is not greater than 4")
+if 34>45:
+    print("34 is greater than 45")
+else:
+     print("34 is not greater than 45")
+      print("hello world")
---
+++ variables.py
@@ -0,0 +1,11 @@
+x=5
+y=10
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+if 5>6:
+    print("5 is greater than 4")
+else:
+    print("5 is not greater than 4")

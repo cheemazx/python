7b3d6f8a62e86c4fd46a4e05cb1d616f38034064
cheemazx <2012341@szabist-isb.pk>
2026-02-12 02:08:54 +0500

    day2.1

---
+++ data-types.py
@@ -0,0 +1,30 @@
+x=1
+print(type(x))
+x=1.1
+print(type(x))
+x=1+2j
+print(type(x))
+x=True
+print(type(x))
+x=False
+print(type(x))
+x="hello"
+print(type(x))
+x=["hello", "world"]
+print(type(x))
+x=("hello", "world")
+print(type(x))
+x={"name": "John", "age": 36}
+print(type(x))
+x={"hello", "world"}
+print(type(x))
+x=range(6)
+print(type(x))
+x=frozenset({"hello", "world"})
+print(type(x))
+x=bytes(5)
+print(type(x))
+x=bytearray(5)
+print(type(x))
+x=memoryview(bytes(5))
+print(type(x))
